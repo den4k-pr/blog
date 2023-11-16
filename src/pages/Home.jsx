45541b32918885a0b22e416f66868b7276c2380d
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-11-16 19:39:40 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import { SectionProject } from "../components/home/sectionProject"
-
+import { SectionProject } from "../components/home/sectionProject";
 
 export const Home = () => {
-    return(
-        <section >
-            <h1 className="title"><span>Кодовый Мир:</span> Проекты и Заметки Программиста</h1>
-            <SectionProject link="emailjs" data="Опубликовано: 10 ноября" name="React.JS, Email.JS отправка формы на почту" text="Как легко настроить систему отправки писем на электронную почту с React.JS"/>
+    return (
+        <section>
+            <h1 className="title"><span>Świat Kodu:</span> Projekty i Notatki Programisty</h1>
+            <SectionProject link="emailjs" data="Opublikowano: 10 listopada" name="React.JS, Email.JS - wysyłanie formularza pocztowego" text="Jak łatwo skonfigurować system wysyłki wiadomości e-mail z React.JS"/>
         </section>
-    )
-}
+    );
+};
