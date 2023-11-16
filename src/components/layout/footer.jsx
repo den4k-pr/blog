b6d1b15beaee7d2bce327e2cd5dda43d1b63320a
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-11-16 19:42:35 +0100

    Update footer.jsx

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export const Footer = () => {
     return(
         <footer className="footer">
-            <span className="footer-text">© 2023 blog-den.vercel.app | <Link to="/privacy">Политика конфиденциальности</Link></span>
+            <span className="footer-text">© 2023 blog-den.vercel.app | <Link to="/privacy">Polityka prywatności</Link></span>
         </footer>
     )
 }
