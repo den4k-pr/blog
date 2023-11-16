ec19b5611180b0e740ba793198f366c424b1e271
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-11-16 19:41:46 +0100

    Update info.jsx

--- src/components/about/info.jsx
+++ src/components/about/info.jsx
@@ -1,10 +1,8 @@
-
-
 export const AboutInfo = () => {
     return(
         <section className="about__info">
-            <h2 className="main--title">Про меня</h2>
-            <p className="main--text">Привет, меня зовут Артем Иванов, я веб-разработчик из Москвы, Россия. Недавно я вступил в мир программирования и создал свой блог. Мой путь в этой области начался всего несколько лет назад (давно!), и с каждым днем я все глубже погружаюсь в этот увлекательный мир. Блог был основан мной всего лишь несколько лет назад, и я полон решимости развивать его. С момента его создания я стараюсь делиться практическими руководствами, примерами рабочего кода и типовыми проектами, которые мне встречаются в процессе моей ежедневной работы. Этот блог стал не просто хобби, а настоящей страстью для меня. В настоящее время я сосредотачиваю большую часть своего времени на развитии блога, а моя цель и мечта заключаются в том, чтобы активно продвигать его и, возможно, в будущем сделать его своим основным занятием. Я горжусь возможностью вносить свой вклад в сообщество веб-разработчиков и надеюсь, что блог будет полезен и вдохновляющим ресурсом для других, как и для меня.</p>
+            <h2 className="main--title">O mnie</h2>
+            <p className="main--text">Cześć, jestem Artem Iwanow, web developer z Moskwy, Rosja. Ostatnio dołączyłem do świata programowania i założyłem swój blog. Moja droga w tej dziedzinie rozpoczęła się zaledwie kilka lat temu (dawno temu!), i z każdym dniem coraz bardziej zagłębiam się w ten fascynujący świat. Blog został założony zaledwie kilka lat temu, i jestem pełen determinacji, aby go rozwijać. Od momentu jego powstania staram się dzielić praktycznymi poradnikami, przykładami kodu oraz typowymi projektami, które napotykam podczas mojej codziennej pracy. Ten blog stał się nie tylko moim hobby, ale prawdziwą pasją. Obecnie większość mojego czasu skupiam na rozwijaniu bloga, a moim celem i marzeniem jest aktywne promowanie go i być może w przyszłości uczynić go moim głównym zajęciem. Jestem dumny z możliwości wniesienia swojego wkładu w społeczność web developerów i mam nadzieję, że blog będzie użytecznym i inspirującym źródłem dla innych, tak jak dla mnie.</p>
         </section>
     )
 }
